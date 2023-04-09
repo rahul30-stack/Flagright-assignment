f1d2fcb09f6ea80acb41455bc22c33166bcd3af6
Rahul bhatia <76240288+rahul30-stack@users.noreply.github.com>
2023-04-10 00:30:39 +0530

    Assignment Done
    Build a table component to display the transactions in a table nicely which supports pagination/sorting/filtering. You will be given a mock api file (attached with this email), create the component in a way it calls to API and populates the table from the data. You need to implement sorting and filtering in both the component and the mock API.
    
    Requirements
    You must use React to implement the component
    Implement the component from scratch
    Table UI is up to you, you can decide to what extent you want to beautify it

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TransactionTable from './components/TransactionTable';
+
+const App = () => {
+  return (
+    <div>
+      <TransactionTable></TransactionTable>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/TransactionTable.js
@@ -0,0 +1,135 @@
+import React, { useState,useEffect,useRef } from "react";
+import { getTransactions } from "./transaction";
+import  './TransactionTable.css';
+
+
+const TransactionTable = () => {
+   
+  const [arrayTransactionState, setarrayTransactionState] = useState([]);
+  
+  
+  const [pageNumberState, setpageNumberState] = useState(
+    Number(localStorage.getItem("pageNumber")) || 1);
+    const [sortOrder, setSortOrder] = useState(false);
+  const [filterValue, setFilterValue] = useState( localStorage.getItem("filterValue") || "");
+  const debounceRef = useRef(false);
+
+  
+
+  useEffect(() => {
+    getTransactions(pageNumberState,sortOrder).then((res) => setarrayTransactionState(res));
+    localStorage.setItem("pageNumber", pageNumberState);
+  }, [pageNumberState,sortOrder]);
+
+
+  // const handleSort = (key) => {
+  //   // setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  //   const sortedArray = arrayTransactionState.data.sort((a, b) => {
+  //     const aValue = a[key].toLowerCase();
+  //     const bValue = b[key].toLowerCase();
+  //     if (sortOrder === "asc") {
+  //       return aValue.localeCompare(bValue);
+  //     } else {
+  //       return bValue.localeCompare(aValue);
+  //     }
+  //   });
+  //   // setarrayTransactionState({ ...arrayTransactionState, data: sortedArray });
+  // };
+ 
+
+  const handleFilter = (e) => {
+   
+    setFilterValue(e.target.value);
+    
+    const filteredArray = arrayTransactionState.data.filter((element) =>element.type&&
+      element.type.toLowerCase().includes(e.target.value.toLowerCase()),
+     
+    );
+    setarrayTransactionState({ ...arrayTransactionState,
+      data: filteredArray?.length ? filteredArray : arrayTransactionState?.data, 
+    });
+    localStorage.setItem("filterValue", e.target.value);
+  };
+   useEffect(() => {
+  const storedValue = localStorage.getItem("filterValue");
+  if (storedValue) {
+    setFilterValue(storedValue);
+  }
+}, []);
+
+
+  const handleReset = () => {
+    setFilterValue("");
+    getTransactions(pageNumberState).then((res) =>
+      setarrayTransactionState(res)
+    );
+    localStorage.removeItem("filterValue");
+  };
+
+
+  return (
+    <div>
+      <div className="site-logo">
+    <img src="https://i.tracxn.com/logo/company/ec2aa1c3512bbd1e562ed655b79e4912?height=120&width=120" alt="logo"></img>
+     </div>
+      <label>Filter by Type:
+        <input type="text" value={filterValue} onChange={handleFilter} /></label>
+        <button className="reset" onClick={handleReset}>Reset</button>
+          <table>
+    <thead>
+      <tr>
+        <th >Transaction id</th>
+        <th>Designation User</th>
+        <th>OriginUserId</th>
+        <th>Type</th>
+        <th>Status</th>
+
+      </tr>
+    </thead>
+    <tbody>
+      {arrayTransactionState?.data?.map((element) => (
+        
+        <tr key={element?.transactionId}>
+          <td>{element?.transactionId}</td>
+          <td>{element?.destinationUserId}</td>
+          <td>{element?. originUserId}</td>
+          <td>{element?. type}</td>
+          <td>{element?. status}</td>
+
+        </tr>
+        
+      ))}
+    </tbody>
+  </table>
+      
+      
+      <button className="aage"
+        onClick={() => {
+          if (debounceRef.current) {
+            clearTimeout(debounceRef.current);
+          }
+          debounceRef.current = setTimeout(() => {
+            setpageNumberState((prevState) => prevState + 1);
+          }, 6000);
+        }}
+      >
+      <p>   Next &#8594; </p>  
+      </button>
+      <button className="aage"
+        onClick={() => {
+          if (debounceRef.current) {
+            clearTimeout(debounceRef.current);
+          }
+          debounceRef.current = setTimeout(() => {
+            setpageNumberState((prevState) => prevState - 1);
+          }, 6000);
+        }}
+      >
+       <p>&#8592; Back </p> 
+      </button>
+      <button className="sort" onClick={() =>setSortOrder((ps)=>!ps)}>Sort by Status</button>
+    </div>
+  );
+  
+}
+export default TransactionTable;
