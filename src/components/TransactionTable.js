eb941bb3acfc5418c7bdf4e16ffc7f523bb6afb7
Rahul bhatia <76240288+rahul30-stack@users.noreply.github.com>
2025-08-20 16:01:18 +0530

    Update TransactionTable.js

--- src/components/TransactionTable.js
+++ src/components/TransactionTable.js
@@ -64,6 +64,13 @@ const TransactionTable = () => {
         <button className="reset" onClick={handleReset}>Clear </button>
           <table>
     <thead>
+     <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N7CDPS25');</script>
+<!-- End Google Tag Manager -->
       <tr>
         <th >Transaction id</th>
         <th>Designation User</th>
@@ -74,6 +81,10 @@ const TransactionTable = () => {
       </tr>
     </thead>
     <tbody>
+     <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N7CDPS25"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
       {arrayTransactionState?.data?.map((element) => (
         
         <tr key={element?.transactionId}>
